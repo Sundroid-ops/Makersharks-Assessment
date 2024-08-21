5c53dcfe7c6f4f19b1f249ef3fb55bae03d87f05
Lipun <lipundani@gmail.com>
2024-08-21 21:23:35 +0530

    create: created supplier controller class with saveSupplier method of POST request to save supplier data

---
+++ src/main/java/com/Internship/Assignment/Controller/SupplierController.java
@@ -0,0 +1,24 @@
+package com.Internship.Assignment.Controller;
+
+import com.Internship.Assignment.DTO.SupplierDTO;
+import com.Internship.Assignment.Entity.Supplier;
+import com.Internship.Assignment.Service.Interface.SupplierService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/supplier")
+public class SupplierController {
+    @Autowired
+    private SupplierService supplierService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Supplier> saveSupplier(@Valid @RequestBody SupplierDTO supplierDTO){
+        return ResponseEntity.ok().body(supplierService.saveSupplier(supplierDTO));
+    }
+}
