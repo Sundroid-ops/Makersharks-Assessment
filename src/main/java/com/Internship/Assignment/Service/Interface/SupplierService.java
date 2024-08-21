3997778e508d3e0c1d03d4d74f15e81ca33156aa
Lipun <lipundani@gmail.com>
2024-08-21 20:40:47 +0530

    create: created supplier service interface

---
+++ src/main/java/com/Internship/Assignment/Service/Interface/SupplierService.java
@@ -0,0 +1,8 @@
+package com.Internship.Assignment.Service.Interface;
+
+import com.Internship.Assignment.DTO.SupplierDTO;
+import com.Internship.Assignment.Entity.Supplier;
+
+public interface SupplierService{
+    public Supplier saveSupplier(SupplierDTO supplierDTO);
+}
