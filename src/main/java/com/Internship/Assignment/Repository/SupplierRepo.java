1c2ebb35201e27a309fbaf56de9bd8020b638f77
Lipun <lipundani@gmail.com>
2024-08-21 19:52:16 +0530

    fix: extending supplierRepo with JPARepository

--- src/main/java/com/Internship/Assignment/Repository/SupplierRepo.java
+++ src/main/java/com/Internship/Assignment/Repository/SupplierRepo.java
@@ -1,7 +1,11 @@
 package com.Internship.Assignment.Repository;
 
+import com.Internship.Assignment.Entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface SupplierRepo {
+public interface SupplierRepo extends JpaRepository<Supplier, UUID> {
 }
