65334ada8b02b7b09753f1b23a0ef33f1af5d40c
Lipun <lipundani@gmail.com>
2024-08-22 12:44:28 +0530

    errors handle: handling invalid incoming requests and sending all errors as a response

--- src/main/java/com/Internship/Assignment/Advice/RestResponseExceptionHandler.java
+++ src/main/java/com/Internship/Assignment/Advice/RestResponseExceptionHandler.java
@@ -3,8 +3,12 @@ package com.Internship.Assignment.Advice;
 import com.Internship.Assignment.Exception.InternalServerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class RestResponseExceptionHandler {
@@ -12,5 +16,14 @@ public class RestResponseExceptionHandler {
     public ResponseEntity<ErrorMessage> internalServerException(InternalServerException internalServerException){
         ErrorMessage error = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, internalServerException.getMessage());
         return ResponseEntity.internalServerError().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlingInvalidRequests(MethodArgumentNotValidException notValidException){
+        Map<String, String> exceptions = new HashMap<>();
+        notValidException.getBindingResult().getFieldErrors().
+                forEach(err -> exceptions.put(err.getField(), err.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(exceptions);
     }
 }
