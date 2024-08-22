e81a3d9494a22e17fab28c25af749ebc86c9f741
Lipun <lipundani@gmail.com>
2024-08-22 13:12:47 +0530

    logs: enabling methods to show logs for RestResponseExceptionHandler.java

--- src/main/java/com/Internship/Assignment/Advice/RestResponseExceptionHandler.java
+++ src/main/java/com/Internship/Assignment/Advice/RestResponseExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.Internship.Assignment.Advice;
 
 import com.Internship.Assignment.Exception.InternalServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -13,9 +15,12 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class RestResponseExceptionHandler {
+
+    private static final Logger logs = LoggerFactory.getLogger(RestResponseExceptionHandler.class);
     @ExceptionHandler(InternalServerException.class)
     public ResponseEntity<ErrorMessage> internalServerException(InternalServerException internalServerException){
         ErrorMessage error = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, internalServerException.getMessage());
+        logs.error("Internal Server Error: {}", error);
         return ResponseEntity.internalServerError().body(error);
     }
 
@@ -25,11 +30,13 @@ public class RestResponseExceptionHandler {
         notValidException.getBindingResult().getFieldErrors().
                 forEach(err -> exceptions.put(err.getField(), err.getDefaultMessage()));
 
+        logs.warn("Invalid Incoming Requests : {}", exceptions);
         return ResponseEntity.badRequest().body(exceptions);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleEnumErrors(HttpMessageNotReadableException notReadableException){
+        logs.warn("Invalid Enums Type : {}", notReadableException.getMessage());
         return ResponseEntity.badRequest().body(notReadableException.getCause().getMessage());
     }
 }
