f87d970ad7eed39b2e863784baf427eda82cf192
Lipun <lipundani@gmail.com>
2024-08-22 13:13:53 +0530

    logs && refactor: enabling logs for SupplierController class and refactoring saveSupplier with info logs

--- src/main/java/com/Internship/Assignment/Controller/SupplierController.java
+++ src/main/java/com/Internship/Assignment/Controller/SupplierController.java
@@ -4,6 +4,8 @@ import com.Internship.Assignment.DTO.SupplierDTO;
 import com.Internship.Assignment.Entity.Supplier;
 import com.Internship.Assignment.Service.SupplierService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +19,14 @@ public class SupplierController {
     @Autowired
     private SupplierService supplierService;
 
+    private static final Logger logs = LoggerFactory.getLogger(SupplierController.class);
+
     @PostMapping("/create")
     public ResponseEntity<Supplier> saveSupplier(@Valid @RequestBody SupplierDTO supplierDTO){
-        return ResponseEntity.ok().body(supplierService.saveSupplier(supplierDTO));
+        logs.info("Incoming Request for saving new Supplier Data : {}", supplierDTO);
+        Supplier savedSupplier = supplierService.saveSupplier(supplierDTO);
+        logs.info("Supplier Succesfully created: {}", savedSupplier);
+
+        return ResponseEntity.ok().body(savedSupplier);
     }
 }
