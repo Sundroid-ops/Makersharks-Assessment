8572d1c8b5a690b92944e003725b11015192afdb
Lipun <lipundani@gmail.com>
2024-08-22 14:28:56 +0530

    error: custom supplier data not found exception for throwing on not found supplier data objects

---
+++ src/main/java/com/Internship/Assignment/Exception/SupplierNotFoundException.java
@@ -0,0 +1,22 @@
+package com.Internship.Assignment.Exception;
+
+public class SupplierNotFoundException extends RuntimeException{
+    public SupplierNotFoundException() {
+    }
+
+    public SupplierNotFoundException(String message) {
+        super(message);
+    }
+
+    public SupplierNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SupplierNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public SupplierNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
