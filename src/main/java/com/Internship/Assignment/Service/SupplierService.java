b4382b488f4ef91db6a05290c1d49608fd847c94
Lipun <lipundani@gmail.com>
2024-08-22 19:35:23 +0530

    feat: service logic to query for suppliers which accepts location, business and manufacture parameters with size and page as well

--- src/main/java/com/Internship/Assignment/Service/SupplierService.java
+++ src/main/java/com/Internship/Assignment/Service/SupplierService.java
@@ -1,12 +1,18 @@
 package com.Internship.Assignment.Service;
 
 import com.Internship.Assignment.DTO.SupplierDTO;
+import com.Internship.Assignment.Entity.Enums.Business;
+import com.Internship.Assignment.Entity.Enums.Manufacture;
 import com.Internship.Assignment.Entity.Supplier;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SupplierService{
     public Supplier saveSupplier(SupplierDTO supplierDTO);
 
     public Supplier getSupplierByID(UUID id);
+
+    public List<Supplier> searchSuppliers
+            (String location, Business business, Manufacture manufacture, int page, int size);
 }
--- src/main/java/com/Internship/Assignment/Service/SupplierServiceImpl.java
+++ src/main/java/com/Internship/Assignment/Service/SupplierServiceImpl.java
@@ -1,6 +1,8 @@
 package com.Internship.Assignment.Service;
 
 import com.Internship.Assignment.DTO.SupplierDTO;
+import com.Internship.Assignment.Entity.Enums.Business;
+import com.Internship.Assignment.Entity.Enums.Manufacture;
 import com.Internship.Assignment.Entity.Supplier;
 import com.Internship.Assignment.Exception.InternalServerException;
 import com.Internship.Assignment.Exception.SupplierNotFoundException;
@@ -8,9 +10,11 @@ import com.Internship.Assignment.Repository.SupplierRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -51,5 +55,23 @@ public class SupplierServiceImpl implements SupplierService {
         logs.info("Supplier found : {}", getSupplier);
 
         return getSupplier;
+    }
+
+    @Override
+    public List<Supplier> searchSuppliers
+            (String location, Business business, Manufacture manufacture, int page, int size) {
+        Page<Supplier> getSuppliersPagination;
+        logs.info("searching Suppliers by location, business and manufacture queries");
+        try {
+            getSuppliersPagination = supplierRepo.getSuppliersByLocationAndBusinessAndManufacture(location, business, manufacture, PageRequest.of(page, size));
+
+        }catch (Exception exception){
+            logs.error("Error occurred during querying for suppliers");
+            throw new InternalServerException("Error while searching suppliers by querying");
+        }
+
+        logs.info("search completed for finding page of suppliers by querying");
+
+        return getSuppliersPagination.getContent();
     }
 }
