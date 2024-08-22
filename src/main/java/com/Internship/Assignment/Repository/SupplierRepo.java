7cbd62939ff1a66adc7673e79b3942388ff0f240
Lipun <lipundani@gmail.com>
2024-08-22 19:34:27 +0530

    feat: suppliers repo query by jpql to search for suppliers data which is paginated and search by queried by location, business and manufacture with page and size

--- src/main/java/com/Internship/Assignment/Repository/SupplierRepo.java
+++ src/main/java/com/Internship/Assignment/Repository/SupplierRepo.java
@@ -1,11 +1,20 @@
 package com.Internship.Assignment.Repository;
 
+import com.Internship.Assignment.Entity.Enums.Business;
+import com.Internship.Assignment.Entity.Enums.Manufacture;
 import com.Internship.Assignment.Entity.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface SupplierRepo extends JpaRepository<Supplier, UUID> {
+    @Query("SELECT s FROM Supplier s WHERE s.location=:location AND s.business=:business AND s.manufacture=:manufacture")
+    Page<Supplier> getSuppliersByLocationAndBusinessAndManufacture
+            (@Param("location") String location, @Param("business") Business business, @Param("manufacture") Manufacture manufacture, Pageable pageable);
 }
